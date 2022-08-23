55d9de71218adb11ddd221a12edf3caa1fed3c86
Arvind Batham <arvindbatham600.com>
2022-08-23 14:10:41 +0530

    added more lines in the useEffect file

--- src/Components/HookUseEffect.js
+++ src/Components/HookUseEffect.js
@@ -2,12 +2,15 @@ import React,{useState, useEffect} from 'react'
 
 function HookUseEffect() {
     const[count,setCount] = useState(0);
+    const[name,nameSetter] = useState('')
     useEffect(() => {
+        console.log("Chaning the title")
         document.title = `you clicked ${count} times`
-    })
+    },[count])
 
   return (
     <div>
+        <input type= "text" value={name} onChange = {(e) => nameSetter(e.target.value)}></input>
         <button onClick={() => setCount(count + 1)}>clicked {count} times</button>
     </div>
   )
