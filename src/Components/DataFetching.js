086d0d9734f1190a446e8cf95556b9c4c9e3888a
Arvind Batham <arvindbatham600.com>
2022-08-26 10:54:23 +0530

    Fetched the data and added a input text field and a button

--- src/Components/DataFetching.js
+++ src/Components/DataFetching.js
@@ -1,27 +1,45 @@
-import React from 'react'
-import axios from 'axios'
-import {useState, useEffect} from 'react'
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function DataFetching() {
-    const[posts,setPosts] = useState([]);
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res =>{
-            console.log(res)
-            setPosts(res.data)
-        })
-        .catch(error => console.log(error))
-    })
-    
+  const [post, setPost] = useState([]);
+  const [id, setId] = useState(1);
+  const [buttonClickid, buttonClickidHandler] = useState(1);
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => {
+        console.log(res);
+        setPost(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, [buttonClickid]);
+
+  
+
   return (
     <div>
-        <ul>
-            {
-                posts.map(post => <li key={post.id}>{post.title}</li>)
-            }
-        </ul>
+      <div>
+        <input
+          type="text"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        ></input>
+        <div>
+          <button onClick={() => buttonClickidHandler(id)}>Get Title</button>
+        </div>
+      </div>
+      {post.title}
     </div>
-  )
+    // <div>
+    //     <ul>
+    //         {
+    //             post.map(post => <li key={post.id}>{post.title}</li>)
+    //         }
+    //     </ul>
+    // </div>
+  );
 }
 
-export default DataFetching
+export default DataFetching;
